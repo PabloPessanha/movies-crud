4851dc812fca5cd657a94e1463e518f097ffa72c
Pablo <pablopessanh@gmail>
2021-04-22 15:27:11 -0300

    added user auth on state

---
+++ src/store/ducks/user.js
@@ -0,0 +1,16 @@
+const Types = {
+  NEW: 'user/NEW',
+};
+
+export default function user(state = '', action) {
+  switch (action.type) {
+    case Types.NEW:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export const Creators = {
+  newUser: (usr) => ({ type: Types.NEW, payload: usr }),
+};
