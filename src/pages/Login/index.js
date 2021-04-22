b7d388280a9e03afe0af025e042a468c105c4594
Pablo <pablopessanh@gmail>
2021-04-22 15:21:37 -0300

    login page logic

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+// import { useDispatch } from 'react-redux';
 
 function Login() {
+  const [username, setUsername] = useState('');
+  // const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUsername('');
+    history.push('/movies');
+  };
   return (
-    <div>
-      <h1>Login route</h1>
-    </div>
+    <form>
+      <label htmlFor="username-input">
+        Username
+        <input type="text" id="username-input" onChange={(e) => setUsername(e.target.value)} value={username} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>
+        Enviar
+      </button>
+    </form>
   );
 }
 
